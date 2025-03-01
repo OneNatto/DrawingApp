f6fd20c66887e6fd0dc406e2aed561abec4ef743
OneNatto <segaryo0204@icloud.com>
2025-03-01 20:18:47 +0900

    ホーム画面のたたき台のUI作成

--- app/src/main/java/com/example/drawingapp/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/drawingapp/ui/home/HomeScreen.kt
@@ -1,7 +1,40 @@
 package com.example.drawingapp.ui.home
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.drawingapp.ui.navigation.Routes
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(
+    navController: NavController,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+    ) {
+        Button(
+            onClick = {
+                navController.navigate(Routes.WordChainMainScreen.route)
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.DarkGray
+            ),
+            modifier = Modifier.fillMaxWidth(0.8F)
+        ) {
+            Text(
+                "スタート",
+                fontSize = 22.sp
+            )
+        }
+    }
 }
